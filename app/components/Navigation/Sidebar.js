d6d56ab9d563d9876316f81b3087383038ba3d74
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2017-07-08 18:26:13 +0300

    Positioning social icon links

--- app/components/Navigation/Sidebar.js
+++ app/components/Navigation/Sidebar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 const NavLink = require('react-router-dom').NavLink;
+import SocialLink from './SocialLink';
+
 
 class Sidebar extends React.Component {
   constructor(props) {
@@ -42,7 +44,13 @@ class Sidebar extends React.Component {
               </NavLink>
             </li>
           </ul>
+          <div className='social-links'>
+            <SocialLink url='https://www.facebook.com/catedra.ru' imageName='facebook-logo-round.svg' alt='Facebook' />
 
+            <SocialLink url='https://vk.com/catedra' imageName='vk-social-logotype-black.svg' alt='VK' />
+
+            <SocialLink url='https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%B1%D0%BE%D1%80_%D0%9D%D0%B5%D0%BF%D0%BE%D1%80%D0%BE%D1%87%D0%BD%D0%BE%D0%B3%D0%BE_%D0%97%D0%B0%D1%87%D0%B0%D1%82%D0%B8%D1%8F_%D0%9F%D1%80%D0%B5%D1%81%D0%B2%D1%8F%D1%82%D0%BE%D0%B9_%D0%94%D0%B5%D0%B2%D1%8B_%D0%9C%D0%B0%D1%80%D0%B8%D0%B8_(%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0)' imageName='wikipedia-logo_black.svg' alt='Wiki' />
+          </div>
         </nav>
       </div>
       </div>
--- app/components/Navigation/SocialLink.js
+++ app/components/Navigation/SocialLink.js
@@ -4,15 +4,15 @@ class SocialLink extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      image: this.props.image,
       url: this.props.url,
+      imageName: this.props.imageName,
       alt: this.props.alt
     }
   }
 
   render() {
     return (
-      <a href={this.state.url} target='blank'><img className='svg-icon' src={require('../../images/icons/' + this.state.image)} alt={this.state.alt} /></a>
+      <a href={this.state.url} target='blank'><img className='icons' src={require('../../images/icons/' + this.state.imageName)} alt={this.state.alt} /></a>
     )
   }
 }
