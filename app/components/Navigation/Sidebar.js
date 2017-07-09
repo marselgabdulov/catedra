24a6ab5f9688c0f5c49b90906b987848f1bb9d45
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2017-07-09 10:05:06 +0300

    change social-icons color

--- app/components/Navigation/Sidebar.js
+++ app/components/Navigation/Sidebar.js
@@ -45,11 +45,11 @@ class Sidebar extends React.Component {
             </li>
           </ul>
           <div className='social-links'>
-            <SocialLink url='https://www.facebook.com/catedra.ru' imageName='facebook-logo-round.svg' alt='Facebook' />
+            <SocialLink url='https://www.facebook.com/catedra.ru' imageName='facebook-logo-round-red.svg' alt='Facebook' />
 
-            <SocialLink url='https://vk.com/catedra' imageName='vk-social-logotype-black.svg' alt='VK' />
+            <SocialLink url='https://vk.com/catedra' imageName='vk-social-red.svg' alt='VK' />
 
-            <SocialLink url='https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%B1%D0%BE%D1%80_%D0%9D%D0%B5%D0%BF%D0%BE%D1%80%D0%BE%D1%87%D0%BD%D0%BE%D0%B3%D0%BE_%D0%97%D0%B0%D1%87%D0%B0%D1%82%D0%B8%D1%8F_%D0%9F%D1%80%D0%B5%D1%81%D0%B2%D1%8F%D1%82%D0%BE%D0%B9_%D0%94%D0%B5%D0%B2%D1%8B_%D0%9C%D0%B0%D1%80%D0%B8%D0%B8_(%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0)' imageName='wikipedia-logo_black.svg' alt='Wiki' />
+            <SocialLink url='https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%B1%D0%BE%D1%80_%D0%9D%D0%B5%D0%BF%D0%BE%D1%80%D0%BE%D1%87%D0%BD%D0%BE%D0%B3%D0%BE_%D0%97%D0%B0%D1%87%D0%B0%D1%82%D0%B8%D1%8F_%D0%9F%D1%80%D0%B5%D1%81%D0%B2%D1%8F%D1%82%D0%BE%D0%B9_%D0%94%D0%B5%D0%B2%D1%8B_%D0%9C%D0%B0%D1%80%D0%B8%D0%B8_(%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0)' imageName='wikipedia-logo-red.svg' alt='Wiki' />
           </div>
         </nav>
       </div>
