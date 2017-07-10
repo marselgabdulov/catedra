4166d771fed76a4b27059ca5babe3f74daedca59
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2017-07-10 17:09:09 +0300

    Recreate footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,33 +5,52 @@ import Artebene from '../images/artebene.jpeg';
 import Liturgia from '../images/liturgia.jpeg';
 import NewsVa from '../images/news_va.png';
 import PokrovskieVorota from '../images/pokrovskie.png';
-
+const NavLink = require('react-router-dom').NavLink;
 
 class Footer extends Component {
   render() {
     return (
       <footer>
-        <div className='main'>
-          <a href='http://cathmos.ru/' target='_blank' >
-            <img src={Archdiocese} />
-          </a>
+        <div className='contact-info'>
+          <div>
+            <a href='mailto:catedra.moscow@gmail.com'>catedra.moscow@gmail.com</a><br/>
+            <a href='tel:+74992523911'>+7 499 252-39-11</a><br/>
+            <a href='tel:+74992524051'>+7 499 252-40-51</a>
+          </div>
+        </div>
+
+        <div className='address'>
+          <NavLink onClick={this.closeSidebar} exact activeClassName='active' to='/contacts'>
+            карта проезда
+          </NavLink>
+          <div className="literal">
+          Малая Грузинская д.27/13<br/>
+          123557 Москва</div>
+          <br/>
+
+        </div>
+
+        <div className="partners">
           <a href='http://deboniarte.org/' target='_blank'>
-            <img src={DeBoniArte} />
+            Искусство добра
+          </a><br/>
+          <a href='http://mic-minsk.by/breviaryj-new.html' target='_blank'>
+            Литургия часов
+          </a><br/>
+          <a href='http://cathmos.ru/' target='_blank' >
+            Архиепархия
           </a>
         </div>
 
-        <div className='bottom'>
-          <a href='http://mic-minsk.by/breviaryj-new.html' target='_blank'>
-            <img src={Liturgia} />
-          </a>
+        <div className="partners-2">
           <a href='http://www.dbiblio.org/' target='_blank'>
-            <img src={PokrovskieVorota} />
-          </a>
+            покровские ворота
+          </a><br/>
           <a href='http://www.news.va/en' target='_blank'>
-            <img src={NewsVa} />
-          </a>
+            новости ватикана
+          </a><br/>
           <a href='https://artbene.ru/' target='_blank'>
-            <img src={Artebene} />
+            концерты
           </a>
         </div>
       </footer>
