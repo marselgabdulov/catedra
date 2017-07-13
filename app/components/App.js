f7adbc0bfa709159bc9c2815ffa4e6155d9f7715
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2017-07-13 11:30:34 +0300

    Delete requisites components

--- app/components/App.js
+++ app/components/App.js
@@ -4,14 +4,12 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Contacts from './Contacts';
 import Schedule from './Schedule';
-import Requisites from './Requisites';
 import PhotoGallery from './PhotoGallery';
 import Calendar from './Calendar';
 import Sacramentum from './Sacramentum';
 import Ministrant from './Ministrant';
 
 import Parish from './Parish';
-
 
 import Navigation from './Navigation/Navigation';
 import Footer from './Footer';
@@ -21,29 +19,22 @@ class App extends React.Component {
     return (
       <div>
         <BrowserRouter>
-
         <div>
-
           <Navigation />
-
           <div className='content'>
             <Switch>
-                <Route exact path='/' component={Home} />
-
-                <Route exact path='/contacts' component={Contacts} />
-                <Route exact path='/schedule' component={Schedule} />
-                <Route exact path='/requisites' component={Requisites} />
-                <Route exact path='/calendar' component={Calendar} />
-                <Route exact path='/photogallery' component={PhotoGallery} />
-                <Route exact path='/parish' component={Parish} />
-                <Route path='/sacramentum' component={Sacramentum} />
-
-                  <Route exact path='/ministrant' component={Ministrant} />
-
-                <Route render={function () {
-                  return <p>Not Found</p>
-                } } />
-
+              <Route exact path='/' component={Home} />
+              <Route exact path='/contacts' component={Contacts} />
+              <Route exact path='/schedule' component={Schedule} />
+              <Route exact path='/requisites' component={Requisites} />
+              <Route exact path='/calendar' component={Calendar} />
+              <Route exact path='/photogallery' component={PhotoGallery} />
+              <Route exact path='/parish' component={Parish} />
+              <Route path='/sacramentum' component={Sacramentum} />
+              <Route exact path='/ministrant' component={Ministrant} />
+              <Route render={function () {
+                return <p>Not Found</p>
+              } } />
           </Switch>
           </div>
           <Footer />
