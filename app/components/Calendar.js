d30faa91fbaff6846595f159b263c99915cc1074
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2017-07-13 12:00:43 +0300

    Change calendar size

--- app/components/Calendar.js
+++ app/components/Calendar.js
@@ -4,9 +4,13 @@ class Calendar extends React.Component {
 
   render() {
     return (
+      <div>
+        <h1>Календарь событий</h1>
+
       <div id="calendar-page">
-          <iframe id='calendar' src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=acht97kleocgk125jl1fnibe7k%40group.calendar.google.com&amp;color=%238C500B&amp;ctz=Europe%2FMoscow">
+        <iframe id='calendar' src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=acht97kleocgk125jl1fnibe7k%40group.calendar.google.com&amp;color=%238C500B&amp;ctz=Europe%2FMoscow">
         </iframe>
+      </div>
       </div>
     )
   }
